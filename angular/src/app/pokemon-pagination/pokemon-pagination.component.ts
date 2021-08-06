a4eef38d0ddf4d3bc0ca9a29bd668da7bd8b445d
Cole Boggus <cole@pcgamingrace.com>
2021-08-06 02:31:15 -0500

    Added search

--- angular/src/app/pokemon-pagination/pokemon-pagination.component.ts
+++ angular/src/app/pokemon-pagination/pokemon-pagination.component.ts
@@ -10,7 +10,8 @@ export class PokemonPaginationComponent implements OnInit {
 
   page = 1;
   pageSize = 10;
-  @Input() pokemonData: any;
+  pokemonData: any = {};
+  allPokemonData:any = {};
   count = 0;
   active:Array<any> = [];
   constructor() { }
@@ -18,13 +19,19 @@ export class PokemonPaginationComponent implements OnInit {
   ngOnInit(): void {
     fetch(`http://localhost:3000/pokemon/count`).then(r => r.json().then(r => this.count = r))
     fetch(`http://localhost:3000/pokemon?limit=2000`).then(r => r.json().then(r => {
-      this.pokemonData = r
+      this.pokemonData = r;
+      this.allPokemonData = [...r.results];
       this.setActive();
     })).catch();
   }
 
-  grabPokemonDetails(pokemon:any):void{
-
+  searchPokemon($event:Event):void{
+    // @ts-ignore
+    let query = $event.target.value;
+    // @ts-ignore
+    this.pokemonData.results = !query ? [...this.allPokemonData] : this.allPokemonData.filter((pokemon:any) => pokemon.name.includes(query));
+    this.page = 1;
+    this.setActive();
   }
 
   expandPokemon(pokemon:any):void{
@@ -36,9 +43,11 @@ export class PokemonPaginationComponent implements OnInit {
       console.log(r);
       pokemon.expanded = {weight: r.weight,generation:r.game_indices[0].version.name, moves: r.moves}
     }))
+
   }
 
   setActive() {
     this.active = this.pokemonData.results.filter((p:any,id:any) => id >= this.page*this.pageSize-9 && id < this.pageSize+this.page*this.pageSize-9 );
   }
+
 }
